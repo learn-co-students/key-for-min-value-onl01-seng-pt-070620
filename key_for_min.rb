a64f040fa921ffa7b0d9331adde45b15c55858d4
Nasrin Rahimi <77.nasrin@gmail.com>
2020-07-16 23:02:44 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,14 +5,14 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     nil
   else
-    min_value = 1000
-    min_key = " "
+    min_key = nil
+    min_value = nil
     name_hash.collect do |key,value|
-     if min_value > value
-       min_value = value
-       min_key = key
-     end
-   end
-   min_key
- end
+      if min_value == nil || value < min_value
+        min_key = key
+        min_value = value
+      end
+    end
+    min_key
+  end
 end
